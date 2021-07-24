16b0821ec6f02e7d1e76a644111adf44e7cf0215
Oliver's Alicia <ohohdarkwolf@gmail.com>
2021-07-24 15:53:23 +0700

    pre localStorage madness

--- script.js
+++ script.js
@@ -1,66 +1,90 @@
 const cout = console.log
+// localStorage loading
+let in_n_compeTasks = "{'Incomplete': [], 'Complete': []}"
+const inComAcc = JSON.parse(in_n_compeTasks)
 
 const charCodeCheck = (eve) => {
     if ((eve.which || eve.keyCode) === 13) EnteringInput()
 }
 
-const ButtonAppear = () => {
-    cout("Taskbox: you here, hi!!")
-}
-
 function emptyCheck(input) {
-    if (input == "" || input == " ") {
+    if (input.length == 0 || input.indexOf(' ') >= 0) {
         alert("Cannot add empty task")
         return false
     }
     return true
 }
 
-function clickAction() {
-    cout("Taskbox: you just click")
-}
+const taskbxCons = (TaskCalled) => {
+        const taskTe = document.createElement('p')
+        const taskBx = document.createElement('div')
+        const taskActionBar = document.createElement('div')
+        const taskDone = document.createElement('button')
+        const taskDel = document.createElement('button')
 
-const EnteringInput = () => {
-    cout("JS: you just add some thing")
-    //element creation
-    const taskIn = document.getElementById("TaskInput").value
-    const taskTe = document.createElement('p')
-    const taskBx = document.createElement('dir')
-    const taskDone = document.createElement('button')
-    const taskDel = document.createElement('button')
+        const DoneAction = () => {
+            document.getElementById("CompletedBox").append(taskTe)
+            taskTe.className = "mainFont text-lg text-center p-1 line-through"
 
-    const ButtonAppear = () => {
-        cout("Taskbox: Button Appear")
-        taskBx.append(taskDone)
-        taskBx.append(taskDel)
-    }
+            taskBx.remove()
+        }
 
-    const ButtonGone = () => {
-        cout("Taskbox: Button Disappear")
-        taskBx.removeChild(taskBx.childNodes[2])
-        taskBx.removeChild(taskBx.childNodes[1])
-    }
+        const DeleteAction = () => {
+            taskBx.remove()
+        }
 
-    if (emptyCheck(taskIn)) {
-        //Style and text innit
-        taskBx.style.border = "thin solid black"
-        taskTe.style.textAlign = "center"
-        taskTe.innerHTML = taskIn
+        const ButtonAppear = () => {
+            taskDone.style.visibility = "visible"
+            taskDel.style.visibility = "visible"
+        }
+
+        const ButtonGone = () => {
+            taskDone.style.visibility = "hidden"
+            taskDel.style.visibility = "hidden"
+        }
+
+        taskBx.className = "max-w-md mx-auto p-1 border flex flex-col"
+        taskTe.className = "mainFont text-lg text-center p-1"
+        taskActionBar.className = "flex flex-row justify-center space-x-2"
+
+        taskDel.className = "p-0.5 border"
+        taskDel.style.visibility = "hidden"
+        taskDone.className = "p-0.5 border"
+        taskDone.style.visibility = "hidden"
+
+        taskTe.innerHTML = TaskCalled
         taskDone.innerHTML = "Done"
-        taskDone.style.alignItems = "center"
         taskDel.innerHTML = "Delete"
-        taskDel.style.alignItems = "space-between"
 
         //Add eventlistener
         taskBx.addEventListener('mouseenter', ButtonAppear)
         taskBx.addEventListener('mouseleave', ButtonGone)
-        taskDone.addEventListener("click", clickAction)
-        taskDel.addEventListener("click", clickAction)
+        taskDone.addEventListener("click", DoneAction)
+        taskDel.addEventListener("click", DeleteAction)
 
         //Put thing in place
         taskBx.append(taskTe)
-        document.getElementById("TaskBox").append(taskBx)
+        taskBx.append(taskActionBar)
+        taskActionBar.append(taskDone)
+        taskActionBar.append(taskDel)
+        document.getElementById("TaskBox").prepend(taskBx)
 
+}
+
+const complTaskadding = (complTask) => {
+    const taskTe = document.createElement('p')
+    taskTe.innerHTML = complTask
+    document.getElementById("CompletedBox").append(taskTe)
+    taskTe.className = "mainFont text-lg text-center p-1 line-through"
+}
+
+
+const EnteringInput = () => {
+    cout("JS: you just add some thing")
+    const taskIn = document.getElementById("TaskInput").value
+
+    if (emptyCheck(taskIn)) {
+        taskbxCons(taskIn)
         //clear old text
         document.getElementById("TaskInput").value = ""
     }
